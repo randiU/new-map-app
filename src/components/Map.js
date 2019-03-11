1b24b4e8d06997744c5edda311f9eb1c690125d8
randiU <33576756+randiU@users.noreply.github.com>
2019-03-11 14:36:34 -0600

    added google maps api to Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class Map extends Component {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class MapContainer extends Component {
 	render() {
 		return (
-			<div>
-				<p>This will be a map</p>
-			</div>
+			<Map
+		        google={this.props.google}
+		        zoom={14}
+		        style={mapStyles}
+		        initialCenter={{
+		         lat: 43.6169361,
+		         lng: -116.2053802
+		        }}
+		      />
 			)
 	}
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDd7-K4U0STJIqxo--RD9D_XHTuUx1VJ8s'
+})(MapContainer);
