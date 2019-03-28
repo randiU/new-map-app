958350ffb7030a789bd95140a47c23168d0323a4
randiU <33576756+randiU@users.noreply.github.com>
2019-03-28 15:33:27 -0600

    Added images file to src

--- src/components/VenueInformation.js
+++ src/components/VenueInformation.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const VenueInformation = ({currentVenue, activeVenueLocation}) => {
+
+    const donutOrCoffeePic = (venue) => {
+    	if (venue) {
+	    		if (venue.toLowerCase().includes("donuts")) {
+	    		return console.log('donuts');
+	    	} else if (venue.toLowerCase().includes("coffee")) {
+	    		return console.log('coffee');
+	    	} else {
+	    		return console.log('neither');
+	    	}
+    	}
+    }
+
 	if (currentVenue) {
 		return (
 			<div>
 				<p>{currentVenue.name}</p>
 				<p>{activeVenueLocation}</p>
+				<p>{donutOrCoffeePic(currentVenue.name)}</p>
 			</div>
 			)
 	} else {
